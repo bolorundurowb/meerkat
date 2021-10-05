4e61be5ca0d23f947b7abc1e6065764e0b7c4e22
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-05 15:38:17 +0100

    ft: add string and type extensions

---
+++ Meerkat/Extensions/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Meerkat.Services;
+
+namespace Meerkat.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static string Pluralize(this string singular)
+        {
+            if (string.IsNullOrWhiteSpace(singular))
+                return singular;
+
+            return PluralizationService.Pluralize(singular);
+        }
+
+        public static string ReplaceLastOccurrence(this string input, string oldValue, string newValue)
+        {
+            var place = input.LastIndexOf(oldValue, StringComparison.OrdinalIgnoreCase);
+
+            if (place == -1)
+                return input;
+
+            return input.Remove(place, oldValue.Length).Insert(place, newValue);
+        }
+    }
+}
---
+++ Meerkat/Extensions/TypeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using Meerkat.Attributes;
+
+namespace Meerkat.Extensions
+{
+    internal static class TypeExtensions
+    {
+        public static string GetName(this Type type)
+        {
+            var collectionAttribute = type.GetCustomAttribute<CollectionAttribute>();
+            return collectionAttribute?.Name ?? type.Name;
+        }
+        
+        public static bool ShouldTrackTimestamps(this Type type)
+        {
+            var collectionAttribute = type.GetCustomAttribute<CollectionAttribute>();
+            return collectionAttribute?.TrackTimestamps ?? false;
+        }
+    }
+}

bfb318e62630d321eec6bf752b3f652334135539
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-05 14:54:24 +0100

    ft: add in the configuration attributes

---
+++ Meerkat/Attributes/CollectionAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Meerkat.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CollectionAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public bool TrackTimestamps { get; private set; }
+
+        public CollectionAttribute(string collectionName, bool trackTimestamps)
+        {
+            Name = collectionName;
+            TrackTimestamps = trackTimestamps;
+        }
+    }
+}

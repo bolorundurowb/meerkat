ea1165aed524d13466568c716883d04fca5f1e3c
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-06 15:29:17 +0100

    ft: finalize surface area

--- meerkat/Meerkat.cs
+++ meerkat/Meerkat.cs
@@ -105,7 +105,7 @@ namespace meerkat
             return _database.GetCollection<TSchema>(collectionName);
         }
 
-        internal static IMongoCollection<TSchema> GetCollectionForType<TSchema>() where TSchema : Schema
+        private static IMongoCollection<TSchema> GetCollectionForType<TSchema>() where TSchema : Schema
         {
             if (_database == null)
                 throw new InvalidOperationException(

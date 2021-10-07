7939b07672ed8eb1d36ad382cc6d25440f3c1fcd
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-07 15:40:48 +0100

    ft: further normalize the collection names

--- meerkat/Extensions/TypeExtensions.cs
+++ meerkat/Extensions/TypeExtensions.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using meerkat.Attributes;
 
 namespace meerkat.Extensions
 {
     internal static class TypeExtensions
     {
+        private static readonly Regex Whitespace = new Regex("\\s+", RegexOptions.Compiled);
+        
         public static string GetCollectionName(this Type type)
         {
             var collectionAttribute = type.GetCustomAttribute<CollectionAttribute>();
-            return (collectionAttribute?.Name ?? type.Name.Pluralize()).ToLowerInvariant();
+            var attributeName = collectionAttribute?.Name;
+            var name = string.IsNullOrWhiteSpace(attributeName) ? type.Name.Pluralize() : attributeName;
+            return Whitespace.Replace(name.ToLowerInvariant(), "_");
         }
         
         public static bool ShouldTrackTimestamps(this Type type)

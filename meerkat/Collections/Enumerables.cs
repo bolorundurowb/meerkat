92406a14122aa4050367cc33e2c7ce8d9cfde5da
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-07 22:21:42 +0100

    ft: add sync support for bulk inserts

--- meerkat/Collections/Enumerables.cs
+++ meerkat/Collections/Enumerables.cs
@@ -7,16 +7,24 @@ namespace meerkat.Collections
 {
     public static class Enumerables
     {
+        private static readonly InsertManyOptions BulkInsertOptions = new InsertManyOptions
+        {
+            IsOrdered = false,
+            BypassDocumentValidation = true
+        };
+
+        public static void SaveAll<TSchema>(this IEnumerable<TSchema> entities,
+            CancellationToken cancellationToken = default) where TSchema : Schema
+        {
+            var collection = Meerkat.GetCollectionForType<TSchema>();
+            collection.InsertMany(entities, BulkInsertOptions, cancellationToken);
+        }
+
         public static async Task SaveAllAsync<TSchema>(this IEnumerable<TSchema> entities,
             CancellationToken cancellationToken = default) where TSchema : Schema
         {
             var collection = Meerkat.GetCollectionForType<TSchema>();
-            var options = new InsertManyOptions
-            {
-                IsOrdered = false,
-                BypassDocumentValidation = true
-            };
-            await collection.InsertManyAsync(entities, options, cancellationToken);
+            await collection.InsertManyAsync(entities, BulkInsertOptions, cancellationToken);
         }
     }
 }

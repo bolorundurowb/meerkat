01f3650850d5c623448e91c524325ac6d8654ffe
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2021-10-08 14:44:05 +0100

    ft: use caching for the collction names and user tracking choice

--- meerkat/Extensions/TypeExtensions.cs
+++ meerkat/Extensions/TypeExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using meerkat.Attributes;
 
@@ -7,20 +10,42 @@ namespace meerkat.Extensions
 {
     internal static class TypeExtensions
     {
+        private static readonly ConcurrentDictionary<string, string> CollectionNameCache = new ConcurrentDictionary<string, string>();
+        private static readonly ConcurrentDictionary<string, bool> TimestampTrackCache = new ConcurrentDictionary<string, bool>();
         private static readonly Regex Whitespace = new Regex("\\s+", RegexOptions.Compiled);
         
         public static string GetCollectionName(this Type type)
         {
+            var cacheKey = type.FullName ?? type.Name;
+
+            if (CollectionNameCache.ContainsKey(cacheKey))
+                return CollectionNameCache[cacheKey];
+            
             var collectionAttribute = type.GetCustomAttribute<CollectionAttribute>();
             var attributeName = collectionAttribute?.Name;
             var name = string.IsNullOrWhiteSpace(attributeName) ? type.Name.Pluralize() : attributeName;
-            return Whitespace.Replace(name.ToLowerInvariant(), "_");
+            var collectionName = Whitespace.Replace(name.ToLowerInvariant(), "_");
+            
+            // cache this generated name
+            CollectionNameCache[cacheKey] = collectionName;
+
+            return collectionName;
         }
         
         public static bool ShouldTrackTimestamps(this Type type)
         {
+            var cacheKey = type.FullName ?? type.Name;
+
+            if (TimestampTrackCache.ContainsKey(cacheKey))
+                return TimestampTrackCache[cacheKey];
+            
             var collectionAttribute = type.GetCustomAttribute<CollectionAttribute>();
-            return collectionAttribute?.TrackTimestamps ?? false;
+            var shouldTrack =  collectionAttribute?.TrackTimestamps ?? false;
+            
+            // cache the tracking option
+            TimestampTrackCache[cacheKey] = shouldTrack;
+
+            return shouldTrack;
         }
     }
 }

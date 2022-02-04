065d26c9835ab3db518ac126e13712537a146d96
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-02-04 14:24:03 +0100

    ft: add ignore support

---
+++ meerkat/Attributes/IgnoreUnknownAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace meerkat.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class IgnoreUnknownAttribute : BsonIgnoreExtraElementsAttribute
+    {
+    }
+}

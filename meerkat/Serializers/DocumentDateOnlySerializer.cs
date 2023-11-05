c59a1a3a219ac37d671693042e9e985138256774
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2023-11-05 17:03:14 +0100

    ft: add in date only anbd time only serializers

---
+++ meerkat/Serializers/DocumentDateOnlySerializer.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Serializers;
+using System;
+using MongoDB.Bson.Serialization;
+
+namespace meerkat.Serializers
+{
+#if NET6_0 || NET7_0 || NET8_0
+    internal class DocumentDateOnlySerializer : StructSerializerBase<DateOnly>
+    {
+        private const string SerializationFormat = "yyyy-MM-dd";
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, DateOnly value) => context.Writer.WriteString(value.ToString(SerializationFormat));
+
+        public override DateOnly Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args) => DateOnly.ParseExact(context.Reader.ReadString(), SerializationFormat);
+    }
+#endif
+}
---
+++ meerkat/Serializers/DocumentTimeOnlySerializer.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Serializers;
+using System;
+using MongoDB.Bson.Serialization;
+
+namespace meerkat.Serializers
+{
+#if NET6_0 || NET7_0 || NET8_0
+    internal class DocumentTimeOnlySerializer : StructSerializerBase<TimeOnly>
+    {
+        private IBsonSerializer<TimeSpan> InnerSerializer = new TimeSpanSerializer();
+
+        public override TimeOnly Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
+        {
+            var timespan = InnerSerializer.Deserialize(context, args);
+            return TimeOnly.FromTimeSpan(timespan);
+        }
+
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, TimeOnly value) =>
+            InnerSerializer.Serialize(context, args, value.ToTimeSpan());
+    }
+#endif
+}

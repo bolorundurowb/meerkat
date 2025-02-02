54a1846a38a12290f5aa7147db1e6f2f7e7d9f9d
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2025-02-02 19:23:34 +0100

    ft:mark unnecessary attributes as deprecated

--- meerkat/Attributes/IgnoreIfDefaultAttribute.cs
+++ meerkat/Attributes/IgnoreIfDefaultAttribute.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace meerkat.Attributes
 {
+    [Obsolete("This was a thin wrapper over the BsonIgnoreIfDefault attribute. Use that instead as this will be removed in the next major release.")]
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public sealed class IgnoreIfDefaultAttribute : BsonIgnoreIfDefaultAttribute
     {
--- meerkat/Attributes/IgnoreUnknownFieldsAttribute.cs
+++ meerkat/Attributes/IgnoreUnknownFieldsAttribute.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace meerkat.Attributes
 {
+    [Obsolete("This was a thin wrapper over the BsonIgnoreExtraElements attribute. Use that instead as this will be removed in the next major release.")]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     public sealed class IgnoreUnknownFieldsAttribute : BsonIgnoreExtraElementsAttribute
     {

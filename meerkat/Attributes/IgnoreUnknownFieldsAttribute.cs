2758b3bcc54136e09882d847d0df8f63c845fb36
Bolorunduro Winner-Timothy <bolorundurowb@gmail.com>
2025-02-02 21:04:13 +0100

    ft: remove the deprecated attributes

--- meerkat/Attributes/IgnoreUnknownFieldsAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace meerkat.Attributes;
-
-[Obsolete(
-    "This was a thin wrapper over the BsonIgnoreExtraElements attribute. Use that instead as this will be removed in the next major release.")]
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-public sealed class IgnoreUnknownFieldsAttribute : BsonIgnoreExtraElementsAttribute { }
